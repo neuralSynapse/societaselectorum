81d2a40f31b0ee2edc5abc442de051be34bbef7e
neuralSynapse <secretasocietasectorum@gmail.com>
2026-08-12 02:41:23 -0300

    feat(app): add safe integration configuration

---
+++ app/config.js
@@ -0,0 +1,19 @@
+window.SOCIETAS_CONFIG = Object.freeze({
+  version: "0.1.0",
+  integrationMode: "web-links",
+  publicSite: "https://www.sociedadedoseleitos.com",
+  routes: Object.freeze({
+    home: "https://www.sociedadedoseleitos.com/",
+    library: "https://www.sociedadedoseleitos.com/biblioteca",
+    cartography: "https://www.sociedadedoseleitos.com/cartografia",
+    login: "https://www.sociedadedoseleitos.com/comunidade/entrar",
+    signup: "https://www.sociedadedoseleitos.com/cadastro"
+  }),
+  sync: Object.freeze({
+    auth: false,
+    library: false,
+    progress: false,
+    cartography: false,
+    practices: false
+  })
+});
