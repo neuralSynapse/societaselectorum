ad5ee0ae020012f24238633dbb3af41134d65a6b
neuralSynapse <secretasocietasectorum@gmail.com>
2026-09-09 22:44:21 -0300

    fix: preserve canonical stage power data

--- game/chronica-harun/autoload/CombatLoadoutDirector.gd
+++ game/chronica-harun/autoload/CombatLoadoutDirector.gd
@@ -2,51 +2,28 @@ extends Node
 
 signal active_power_changed(power_id: StringName)
 
-var stage_director: StageDirector
 var active_power: StringName = &""
 
 func _ready() -> void:
     RogueliteContentService.build_changed.connect(_on_build_changed)
-    get_tree().node_added.connect(_on_node_added)
     _on_build_changed(RogueliteContentService.ensure_build())
-    call_deferred("_scan_existing")
-
-func _process(_delta: float) -> void:
-    _apply_active_power()
-
-func _scan_existing() -> void:
-    var scene := get_tree().current_scene
-    if scene != null:
-        _scan_node(scene)
-
-func _scan_node(node: Node) -> void:
-    if node is StageDirector:
-        stage_director = node as StageDirector
-        _apply_active_power()
-        return
-    for child in node.get_children():
-        _scan_node(child)
-        if stage_director != null:
-            return
-
-func _on_node_added(node: Node) -> void:
-    if node is StageDirector:
-        stage_director = node as StageDirector
-        call_deferred("_apply_active_power")
 
 func _on_build_changed(build: Dictionary) -> void:
     var next_power := StringName(build.get("active_power", ""))
     if next_power == active_power:
-        _apply_active_power()
         return
     active_power = next_power
-    _apply_active_power()
     active_power_changed.emit(active_power)
 
-func _apply_active_power() -> void:
-    if active_power == &"" or stage_director == null or not is_instance_valid(stage_director):
-        return
-    if stage_director.stage_data.is_empty():
-        return
-    if StringName(stage_director.stage_data.get("power_id", "")) != active_power:
-        stage_director.stage_data["power_id"] = String(active_power)
+func activate_known_power(power_id: StringName) -> bool:
+    if power_id == &"":
+        return false
+    var build := RogueliteContentService.ensure_build()
+    var known: Array = build.get("powers", [])
+    if not known.has(String(power_id)):
+        return false
+    if StringName(build.get("active_power", "")) == power_id:
+        return true
+    build["active_power"] = String(power_id)
+    RogueliteContentService.build_changed.emit(build.duplicate(true))
+    return true
